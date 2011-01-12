21449298300eb0f500f72d69a499fd056e18406b
Danny Navarro <j@dannynavarro.net>
2011-01-12 09:52:31 +0100

    spacing

--- asyncapp/views.py
+++ asyncapp/views.py
@@ -1,11 +1,10 @@
-import time
 from multiprocessing import Process
 from asyncapp import running
 
 def mi_funcion(running):
     if not running.value:
-	running.value = 1
-	print "Building..."
+        running.value = 1
+        print "Building..."
         import time; time.sleep(10)
         print "Hola chavales"
         running.value = 0
