77c72a753f6e8bdc54052949c2a4838fc34586ee
Danny Navarro <j@dannynavarro.net>
2011-01-18 18:04:03 +0100

    almost

--- asyncapp/views.py
+++ asyncapp/views.py
@@ -1,23 +1,29 @@
-from multiprocessing import Process
-from Queue import Queue
+from multiprocessing import Process, Queue
 
 
 job = 0
 q = Queue(maxsize=2)
+p = Process()
 
 def work():
-    global job
-    job = q.get()
+    global p
     import time; time.sleep(8)
+    job = q.get()
     print("Job done: {0}".format(job))
+    if q.qsize > 0:
+        p = Process(target=work)
+        p.start()
 
 def my_view(request):
     global job
     if not q.full():
         job += 1
         q.put_nowait(job)
-        print("Job submited: {0}".format(job))
-        Process(target=work, args=(q,)).start()
+        global p
+        if not p.is_alive():
+            p = Process(target=work)
+            p.start()
+        print("Job submitted: {0}".format(job))
     else:
         print("Queue is full")
     return {'project':'asyncapp'}
