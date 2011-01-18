468f1b85f4451988915f0a2e44efa7d6983f0cc4
Danny Navarro <j@dannynavarro.net>
2011-01-18 21:01:18 +0100

    Made it work exactly as expected with Value

--- asyncapp/views.py
+++ asyncapp/views.py
@@ -1,23 +1,36 @@
-from multiprocessing import Process
-from Queue import Queue
+from multiprocessing import Process, Lock, Queue, Value
 
 
 job = 0
 q = Queue(maxsize=2)
+lock = Lock()
+running = Value('B', 0)
 
 def work():
     global job
-    job = q.get()
     import time; time.sleep(8)
+    job = q.get()
     print("Job done: {0}".format(job))
+    print(q.qsize())
+    if not q.empty():
+        work()
+    else:
+        running.value = 0
 
 def my_view(request):
     global job
-    if not q.full():
+    if not q.full() and not running.value:
         job += 1
         q.put_nowait(job)
-        print("Job submited: {0}".format(job))
-        Process(target=work, args=(q,)).start()
+        print("Job {0} submitted and working on it".format(job))
+        if not running.value:
+            running.value = 1
+            Process(target=work).start()
+    elif not q.full():
+        job += 1
+        q.put_nowait(job)
+        print("Job {0} submitted while working".format(job))
     else:
         print("Queue is full")
+    print(q.qsize())
     return {'project':'asyncapp'}
