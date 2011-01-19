8a4ac2dff9a93011dd4451747cdd407cb092d64f
Danny Navarro <j@dannynavarro.net>
2011-01-19 10:38:42 +0100

    get rid of value and use only locks

--- asyncapp/views.py
+++ asyncapp/views.py
@@ -1,36 +1,33 @@
-from multiprocessing import Process, Lock, Queue, Value
+from multiprocessing import Process, Lock, Queue
 
 
 job = 0
-q = Queue(maxsize=2)
+q = Queue(maxsize=3)
 lock = Lock()
-running = Value('B', 0)
 
 def work():
     global job
     import time; time.sleep(8)
     job = q.get()
     print("Job done: {0}".format(job))
-    print(q.qsize())
+    print("Queue size: {0}\n".format(q.qsize()))
     if not q.empty():
         work()
     else:
-        running.value = 0
+        lock.release()
 
 def my_view(request):
     global job
-    if not q.full() and not running.value:
+    if not q.full():
         job += 1
         q.put_nowait(job)
         print("Job {0} submitted and working on it".format(job))
-        if not running.value:
-            running.value = 1
+        # Not running
+        if lock.acquire(False):
             Process(target=work).start()
-    elif not q.full():
-        job += 1
-        q.put_nowait(job)
-        print("Job {0} submitted while working".format(job))
+        else:
+            print("Job {0} submitted while working".format(job))
     else:
         print("Queue is full")
-    print(q.qsize())
+    print("Queue size: {0}\n".format(q.qsize()))
     return {'project':'asyncapp'}
